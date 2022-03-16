6b1bc5fa4440fefabe002eb7df3b48b967625c1f
shivi <100689315+shiviPrajapati@users.noreply.github.com>
2022-03-16 17:47:38 +0530

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,8 +1,7 @@
 function reduce(elements, cb, startingValue) {
   let index = startingValue || 0;
   let reducedValue = elements[index];
-
-  for ( let i = index+1; i < elements.length; i++ ) {
+  for ( let i = index+1; i < elements.length; ++i ) {
     reducedValue = cb(reducedValue, elements[i], i );
   }
 

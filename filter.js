73ef06eacd616933f0eb559afca2414c79af60eb
shivi <100689315+shiviPrajapati@users.noreply.github.com>
2022-03-16 17:46:24 +0530

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,7 +1,7 @@
 function filter(elements, cb) {
     let filtered =[];
     for ( let index=0; index< elements.length; index++ ){
-      cb(elements[index]) ? filtered.push(elements[index]) : null;
+      cb(elements[index], index, elements) ? filtered.push(elements[index]) : null;
     }
     return filtered;
   }

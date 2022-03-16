09e006fcdce486d57690235d604bf8ffaee2b264
shivi <100689315+shiviPrajapati@users.noreply.github.com>
2022-03-16 17:47:13 +0530

    Update map.js

--- map.js
+++ map.js
@@ -1,8 +1,8 @@
 function map(elements, cb){
     let mappedArray=[];
     for(let index=0; index<elements.length; index++){
-        mappedArray.push( cb( elements[ index ], index));
-    }   
+        mappedArray.push( cb( elements[ index ], index, elements));
+    }  
     return mappedArray;
 }
 
